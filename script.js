8c7bcb832840e4fdaedf1463fbc584e18b359fbb
TareqZiadd <165607303+TareqZiadd@users.noreply.github.com>
2024-07-14 10:13:33 +0300

    script.js

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
-/* function myDisplayer(some) {
-    document.getElementById("demo").innerHTML = some;
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
 }
 
 function myCalculator(num1, num2) {
-    let sum = num1 + num2;
-    myDisplayer(sum); // call back function 
+  let sum = num1 + num2;
+  myDisplayer(sum); // call back function
 }
 
 myCalculator(5, 5);
+/*
+Output: 
+The content of the HTML element with the id "demo" will be updated to "10".
 */
 
-/*
-function myDisplayer(some) {
-    document.getElementById("demo").innerHTML = some;
-  }
-  
-  function myCalculator(num1, num2, myCallback) {   //call back function in parameter
-    let sum = num1 + num2;
-    myCallback(sum); // Call back the provided callback function with the result
-  }
-  
-  myCalculator(5, 5, myDisplayer);
-  */
 
-/*
-function callback (num){
-    console.log (num *10);
+function callback(num){
+  console.log(num * 10);
 }
 
-
-function myFunction (num,callback){
-
-callback(num);
+function myFunction(num, callback){
+  callback(num);
 }
 
+myFunction(5, callback);
+/*
+Output: 
+50 will be logged to the console.
+*/
 
-myFunction (5,callback);
 
 /*
-Note
-When you pass a function as an argument, remember not to use parenthesis.
+Note:
+When you pass a function as an argument, remember not to use parentheses.
 Right: myCalculator(5, 5, myDisplayer);
 Wrong: myCalculator(5, 5, myDisplayer());
 */
@@ -55,19 +47,22 @@ Where callbacks really shine are in asynchronous functions, where one function h
 Asynchronous functions are covered in the next chapter.
 */
 
-/*
+
 function codeBlocker(){
-    let i = 0;
-  
-    while (i < 1000000000) {
+  let i = 0;
+
+  while (i < 1000000000) {
       i++;
-    }
-    return 'count to billion is complete';
   }
-  
-  console.log('Hello A'); 
-  console.log(codeBlocker());
-  console.log('Hello C');
+  return 'count to billion is complete';
+}
+
+console.log('Hello A'); 
+console.log(codeBlocker());
+console.log('Hello C');
+/*
+Output:
+Hello A
+count to billion is complete
+Hello C
 */
-
-
